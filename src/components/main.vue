9f62ef00aa0639f5eddcc594250d74831f4f4742
sunyu1 <sunyu1@green-valley.com>
2019-10-08 17:13:12 +0800

    add

---
+++ src/components/main.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main">
+    <div class="header">
+      <div class="headerLogo">
+        <img src="" alt="" />
+        <span>燧人科技</span>
+      </div>
+      <div class="headerOperate">
+        <span>首页</span>
+        <span>关于我们</span>
+      </div>
+    </div>
+    <div class="content">
+      <img src="" alt="" />
+    </div>
+    <div class="footer">
+      随人科技 版权所有 2008-2020 苏ICP备15007261号-1
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.main {
+  .header {
+  }
+  .content {
+  }
+  .footer {
+  }
+}
+</style>
